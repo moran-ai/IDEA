733750228ed73848a4158e7c6274598b81841cc1
moran-ai <58198222+moran-ai@users.noreply.github.com>
2021-05-31 12:32:24 +0800

    Delete 复习 directory

--- 复习/局部内部类/recall_Main.java
+++
@@ -1,9 +0,0 @@
-package 复习.局部内部类;
-
-public class recall_Main {
-    public static void main(String[] args) {
-        // 创建外部类对象
-        recall_Outer recall_outer = new recall_Outer();
-        recall_outer.method(); // 间接使用局部内部类
-    }
-}
